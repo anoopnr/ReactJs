5ee94a05279db80e2ebc57a08a9e154992386549
anoopnr <anoopnr1989@gmail.com>
2019-09-21 11:40:56 +0530

    gallery

---
+++ MEAN/server.js
@@ -0,0 +1,12 @@
+try{
+    var express = require('express');
+    var app = express();
+    console.log(__dirname);
+    app.use(express.static(__dirname)); //__dir and not _dir
+    var port = 8000; // you can use any port
+    app.listen(port);
+    console.log('server on' + port);
+}
+catch(ex){
+    console.log(ex);
+}
