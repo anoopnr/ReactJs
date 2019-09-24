7aa85e75dbd70944f27b4b424ea7183784a8d505
anoopnr <50025565+anoopnr@users.noreply.github.com>
2019-09-24 18:07:37 +0530

    Update server.js

--- MEAN/server.js
+++ MEAN/server.js
@@ -3,7 +3,7 @@ try{
     var app = express();
     console.log(__dirname);
     app.use(express.static(__dirname)); //__dir and not _dir
-    var port = 80; // you can use any port
+    var port = 8000; // you can use any port
     app.listen(port);
     console.log('server on' + port);
 }
